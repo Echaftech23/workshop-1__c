125b4cbe7a8aad54efe717971f052ebcc0274f5b
Echaftech23 <rachidchaf2001@gmail.com>
2023-09-15 16:42:56 +0200

    done with challenge 8

--- Les variables/challenge-8.c
+++ Les variables/challenge-8.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Challenge 8 : Affichage de la valeur octale et hexadécimale équivalente. */
-
 int main(int argc, char *argv[]) {
+<<<<<<< HEAD
+	
+	int Num, octPlace = 1, octDigit, oct =0, origNum = Num;
+	
+
+	printf("Veuillez saisir un nombre entier : "); 
+	scanf("%d", &Num);
+	
+	while(Num != 0){
+		octDigit = Num % 8;
+		oct = oct + octDigit * octPlace;
+		octPlace *= 10; 
+		Num /= 8;
+	}
+	
+	printf("La valeur equivalence en octal est : %d\n", oct);
+	printf("La valeur equivalence en hexadecimale est : %X", Num);
+	
+=======
 	float moy, mention;
 	printf("Veuillez saisir une moyenne entre 0 et 20 : ");
 	scanf(" %f", &moy);
@@ -23,6 +40,7 @@ int main(int argc, char *argv[]) {
 	else{
 		printf("Mention tres bien");
 	}
+>>>>>>> 75b7939c8a1b55c3ff55cfcb7625023b802403df
 	
 	return 0;
 }
